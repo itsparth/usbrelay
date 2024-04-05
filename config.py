e92f76d7a6a87e830325957d6d5a7cf226e63fb0
Parth Rathore <parthrathore018@gmail.com>
2024-04-05 18:12:37 +0530

    3.8 config

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+import enum
 from typing import Dict
 
 from diskcache import Cache
@@ -14,37 +15,51 @@ class PinMap:
     green: int
 
 
+class PositionEnum(enum.IntEnum):
+    weapon = 1
+    ammo = 2
+    clearing = 3
+
+    @property
+    def protoEnum(self) -> Position:
+        return {
+            PositionEnum.weapon: Position.Position_weapon,
+            PositionEnum.ammo: Position.Position_ammo,
+            PositionEnum.clearing: Position.Position_clearing,
+        }[self]
+
+
 PositionPinMap: Dict[Position, PinMap] = {
-    Position.Position_weapon: PinMap(buzzer=1, red=2, green=3),
-    Position.Position_ammo: PinMap(buzzer=4, red=5, green=6),
-    Position.Position_clearing: PinMap(buzzer=7, red=8, green=9),
+    PositionEnum.weapon: PinMap(buzzer=1, red=2, green=3),
+    PositionEnum.ammo: PinMap(buzzer=4, red=5, green=6),
+    PositionEnum.clearing: PinMap(buzzer=7, red=8, green=9),
 }
 
 PositionUriMap: Dict[Position, str] = {
-    Position.Position_weapon: "http://localhost:5000",
-    Position.Position_ammo: "http://localhost:5001",
-    Position.Position_clearing: "http://localhost:5002",
+    PositionEnum.weapon: "http://localhost:5000",
+    PositionEnum.ammo: "http://localhost:5001",
+    PositionEnum.clearing: "http://localhost:5002",
 }
 
 DefaultDuration: float = 1
 
-GrpcServerAddress: str = "localhost:50051"
+GrpcServerAddress: str = "localhost:8080"
 
 GrpcChannel = grpc.insecure_channel(GrpcServerAddress)
 
 DeviceIp: str = "192.168.22.55"
 
 PositionPortMap: Dict[Position, int] = {
-    Position.Position_weapon: 5000,
-    Position.Position_ammo: 5001,
-    Position.Position_clearing: 5002,
+    PositionEnum.weapon: 5000,
+    PositionEnum.ammo: 5001,
+    PositionEnum.clearing: 5002,
 }
 
 
 PositionCacheMap: Dict[Position, Cache] = {
-    Position.Position_weapon: Cache("cache/weapon"),
-    Position.Position_ammo: Cache("cache/ammo"),
-    Position.Position_clearing: Cache("cache/clearing"),
+    PositionEnum.weapon: Cache("cache/weapon"),
+    PositionEnum.ammo: Cache("cache/ammo"),
+    PositionEnum.clearing: Cache("cache/clearing"),
 }
 
 MaxEventsFetch = 100
